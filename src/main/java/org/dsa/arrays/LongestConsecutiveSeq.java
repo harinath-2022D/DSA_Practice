66627406584fbfe3e85d29827817808198b5d5e6
Harinath <harireddyakkati.91@gmail.com>
2025-03-03 21:48:16 +0530

    arrays series

---
+++ src/main/java/org/dsa/arrays/LongestConsecutiveSeq.java
@@ -0,0 +1,26 @@
+package org.dsa.arrays;
+
+public class LongestConsecutiveSeq {
+
+    public static void main(String[] args) {
+        int[] arr = {102, 4, 100, 1, 101, 3, 2, 1, 1};
+        brurteForce(arr);
+        better(arr);
+        optimal(arr);
+    }
+
+    private static void optimal(int[] arr) {
+    }
+
+    private static void better(int[] arr) {
+    }
+
+    private static void brurteForce(int[] arr) {
+        int n = arr.length;
+        int cnt = 1;
+
+        for(int i = 0; i < n; i++){
+
+        }
+    }
+}
